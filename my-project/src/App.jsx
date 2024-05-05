a1ad47e1cf82e24690c8fffb267f45886dfe6cfa
18simran <mewannabeauthentic@gmail.com>
2024-05-05 20:48:07 +0530

    Added

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-50">
+      <h1 className="text-xl text-blue-300 font-semibold">
+        Hello world!
+        <br />
+        First code of Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
